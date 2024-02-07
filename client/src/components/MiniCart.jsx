4168ce43ee0bdc67b7ec7568e756d73e1d8c0ed4
geekclick <anujghom27@gmail.com>
2024-02-07 21:12:11 +0530

    create mini review cart

---
+++ client/src/components/MiniCart.jsx
@@ -0,0 +1,43 @@
+import { IoIosCloseCircle } from "react-icons/io";
+import { BsBagX } from "react-icons/bs";
+import minicart from "@/assets/minicart.png";
+import Image from "./Image";
+function MiniCart() {
+  return (
+    <>
+      <div className="flex justify-between items-start w-full">
+        <h1 className="uppercase font-bold text-[18px]">review cart</h1>
+        <BsBagX className=" scale-105 text-[#8D8A94]" />
+      </div>
+      <div className="flex gap-8 justify-between items-start w-full">
+        <div>
+          <Image src={minicart} alt="#" className="w-16 rounded-lg " />
+        </div>
+        <div className="">
+          <h1 className="uppercase">Cloths</h1>
+          <p className=" font-normal text-xs tracking-wide">
+            1 X{" "}
+            <span className="text-[#B88E2F] font-medium">Rs.250,000.00</span>
+          </p>
+        </div>
+        <div>
+          <IoIosCloseCircle className="text-[#8d8a94] scale-150 cursor-pointer" />
+        </div>
+      </div>
+      <div className="flex justify-between items-center w-full mt-36 relative bottom-0">
+        <h1>Subtotal</h1>
+        <p className="text-[#B88E2F] font-medium">Rs. 250,000.00</p>
+      </div>
+      <div className="flex justify-between items-center w-full">
+        <button className="border bg-transparent hover:text-white hover:bg-black transition-all duration-200 text-sm text-black border-black rounded-full px-4 py-1">
+          View Cart
+        </button>
+        <button className="border bg-transparent hover:text-white hover:bg-black transition-all duration-200 text-sm text-black border-black rounded-full px-4 py-1">
+          Checkout
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default MiniCart;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,13 @@ import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { LuSearch, LuHeart, LuShoppingCart } from "react-icons/lu";
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import MiniCart from "./MiniCart";
+
 function Navbar() {
   return (
     <nav className="fixed flex justify-between items-center p-4 w-full h-[52px] bg-[hsl(78,80%,88%)]">
@@ -38,10 +45,19 @@ function Navbar() {
             </Link>
           </li>
           <li className="relative">
-            <LuShoppingCart />
-            <Badge className="absolute -top-2 -right-1 w-3 h-3 p-1 m-auto aspect-square bg-secondary text-white text-[8px]">
-              1
-            </Badge>
+            <Popover>
+              <PopoverTrigger>
+                <>
+                  <LuShoppingCart />
+                  <Badge className="absolute -top-2 -right-1 w-3 h-3 p-1 m-auto aspect-square bg-secondary text-white text-[8px]">
+                    1
+                  </Badge>
+                </>
+              </PopoverTrigger>
+              <PopoverContent className="bg-[#FAFCDB] flex flex-col gap-12 h-fit items-start">
+                <MiniCart />
+              </PopoverContent>
+            </Popover>
           </li>
         </ul>
       </div>
