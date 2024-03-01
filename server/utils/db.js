16b82bed0bebb33f10bc41ef43c6681c001466c5
harishchopade <harishchopde2004>
2024-03-01 23:14:28 +0530

    auth server update

---
+++ server/utils/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const  URL = "mongodb://127.0.0.1:27017/Ecommerce";
+
+
+
+const connectionDB = async ()=>{
+    try {
+        await mongoose.connect(URL);
+        console.log("MongoDB is Connected");
+    } catch (error) {
+        console.error("This connection could not be make",error);
+        process.exit(0);
+    }
+}
+
+module.exports = connectionDB;
