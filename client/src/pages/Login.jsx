66ea7540dc68af3021a6970dab4097d6ccfed3cc
harishchopade <harishchopde2004>
2024-03-01 23:11:36 +0530

    Auth update on clientside

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,201 @@
+// import { Button } from "@/components/ui/button";
+// import { Checkbox } from "@/components/ui/checkbox";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import { FaReact } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import PasswordInput from "@/components/ui/passwordInput";
+// import { useNavigate } from "react-router-dom";
+
+
+
+// function Login() {
+//   const navigate = useNavigate();
+
+//   const [formData, setFormData] = useState({ email: "", password: "" });
+//   const [loginButton, setLoginButton] = useState(false);
+
+//   const handleSubmit = async(e)=>{
+//     e.preventDefault();
+//     if (formData.email !== "" && formData.password !== "") {
+//       setLoginButton(true);
+//     } else {
+//       setLoginButton(false);
+//     }
+
+//     try {
+//       const response = await fetch("http://localhost:5000/api/login", {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(formData),
+//       });
+
+//       if(response.ok){
+//         setFormData({
+//           email: "",
+//           password: ""
+//         });
+//         navigate("/");
+//         console.log("Login Succesfully");
+//         alert("Login Succesfully");
+//       }
+//       console.log(response);
+//     } catch (error) {
+//       console.log("Api coonection error", error);
+//     }
+  
+//   };
+// }
+
+
+//   const handleChange = async (e) => {
+//     const { name, value } = e.target;
+//     console.log(name, value);
+//     setFormData({ ...formData, [name]: value });
+//   }
+
+//   useEffect(() => {
+//     handleSubmit();
+//   }, [formData]);
+
+
+//   return (
+//     <section className="flex justify-left items-center">
+//       <div className="w-[845px] flex-2 h-screen bg-green-200 bg-loginBG bg-contain"></div>
+//       <div className="flex flex-1 flex-col space-y-4 justify-center items-center px-24">
+//         <div className="flex justify-center items-center space-x-2 text-2xl">
+//           <FaReact />
+//           <h1>ABCXYZ</h1>
+//         </div>
+//         <h1 className="font-bold text-xl">Welcome</h1>
+//         <div className="w-full">
+//           <form
+//             onSubmit={handleSubmit}
+//             className="flex justify-start space-y-6 w-full flex-col"
+//           >
+//             <p>Please login here</p>
+//             <div>
+//               <Label>Email Address</Label>
+//               <Input type="email"
+//                 placeholder="JhonMical@example.com"
+//                 name="email"
+//                 value={formData.email}
+//                 onChange={(e) => handleChange(e)}
+//               />
+//             </div>
+//             <div>
+//               <Label>Password</Label>
+//               {/* <Input type="password" 
+//               placeholder="••••••••••••••••••"
+//                /> */}
+//               <PasswordInput
+//                 value={formData.password}
+//                 onChange={(e) => handleChange(e)}
+//               />
+//               <small>Forgot Password?</small>
+//             </div>
+//             <Button
+//               className="w-full"
+//               variant={`${loginButton ? "" : "disabled"}`}
+//               disabled={!loginButton}
+//             >
+//               Log in
+//             </Button>
+//             <div className="flex justify-start space-x-3 items-center">
+//               <Checkbox className="" />
+//               <Label>Remember me</Label>
+//             </div>
+//             {/* <Button>Login</Button> */}
+//           </form>
+//         </div>
+//         <p>
+//           Don't Have Account? <Link to={"/signup"}>Signup</Link>
+//         </p>
+//       </div>
+//     </section>
+//   );
+  
+
+// export default Login
+
+
+
+
+
+
+
+
+
+
+
+
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { FaReact } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import PasswordInput from "@/components/ui/passwordInput";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [loginButton, setLoginButton] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.email !== "" && formData.password !== "") {
+      setLoginButton(true);
+    } else {
+      setLoginButton(false);
+    }
+
+    try {
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setFormData({
+          email: "",
+          password: ""
+        });
+        navigate("/");
+        console.log("Login Succesfully");
+        alert("Login Succesfully");
+      }
+      console.log(response);
+    } catch (error) {
+      console.log("Api coonection error", error);
+    }
+  };
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+
+    if (formData.email !== "" && formData.password !== "") {
+      setLoginButton(true);
+    } else {
+      setLoginButton(false);
+    }
+  };
+
+  useEffect(() => {
+    // handleSubmitButtonState();
+  }, [formData]);
+
   return (
     <section className="flex justify-left items-center">
       <div className="w-[845px] flex-2 h-screen bg-green-200 bg-loginBG bg-contain"></div>
@@ -17,24 +207,38 @@ function Login() {
         <h1 className="font-bold text-xl">Welcome</h1>
         <div className="w-full">
           <form
-            action=""
+            onSubmit={handleSubmit}
             className="flex justify-start space-y-6 w-full flex-col"
           >
             <p>Please login here</p>
             <div>
               <Label>Email Address</Label>
-              <Input type="email" placeholder="JhonMical@example.com" />
+              <Input type="email"
+                placeholder="JhonMical@example.com"
+                name="email"
+                value={formData.email}
+                onChange={(e) => handleChange(e)}
+              />
             </div>
             <div>
               <Label>Password</Label>
-              <Input type="password" placeholder="••••••••••••••••••" />
+              <PasswordInput
+                value={formData.password}
+                onChange={(e) => handleChange(e)}
+              />
               <small>Forgot Password?</small>
             </div>
+            <Button
+              className="w-full"
+              variant={`${loginButton ? "" : "disabled"}`}
+              disabled={!loginButton}
+            >
+              Log in
+            </Button>
             <div className="flex justify-start space-x-3 items-center">
               <Checkbox className="" />
               <Label>Remember me</Label>
             </div>
-            <Button>Login</Button>
           </form>
         </div>
         <p>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,10 +2,94 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import React, { useEffect, useState } from "react";
 import { FaReact } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
+
+
+  const [formData, setFormData] = useState({
+    fn: "",
+    ln: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const [loginButton, setLoginButton] = useState(false);
+  // const [error, setError] = useState({
+  //   phn_error: false,
+  //   password_error: false,
+  // });
+  const handleSubmitButtonState = () => {
+    if (
+      formData.fn.trim() !== "" &&
+      formData.ln.trim() !== "" &&
+      formData.email.trim() !== "" &&
+      formData.password.trim() !== "" 
+    ) {
+      setLoginButton(true);
+    } else {
+      setLoginButton(false);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(formData.phn.length);
+    // if (formData.phn.length === 10) {
+    //   setError({ ...error, phn_error: false });
+    // } else {
+    //   console.log("baby");
+    //   setError({ ...error, phn_error: true });
+    // }
+
+    // if (formData.password === formData.cpassword) {
+    //   setError({ ...error, password_error: false });
+    // } else setError({ ...error, password_error: true });
+
+    // console.log("data is submited safely");
+
+    // Api connection 
+    try {
+
+      const response = await fetch('http://localhost:5000/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      })
+
+      if (response.ok) {
+        setFormData({
+          fn: "",
+          ln: "",
+          email: "",
+          password: ""
+        });
+        navigate('/login');
+      }
+      console.log(response);
+
+    } catch (error) {
+      console.log("Api coonection error", error);
+    }
+
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    // console.log(formData.phn.length);
+  };
+  useEffect(() => {
+    handleSubmitButtonState();
+  }, [formData]);
+
+
+
   return (
     <section className="flex justify-left items-center">
       <div className="w-[845px] flex-2 h-screen bg-green-200 bg-loginBG bg-contain"></div>
@@ -16,28 +100,54 @@ function Signup() {
           <form
             action=""
             className="flex justify-start space-y-6 w-full flex-col mt-6"
+            onSubmit={handleSubmit}
           >
             <div>
               <Label>First Name</Label>
-              <Input type="text " placeholder="Jhon" />
+              <Input type="text "
+                placeholder="Jhon"
+                name="fn"
+                value={formData.fn}
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label>Last Name</Label>
-              <Input type="text " placeholder="Mical" />
+              <Input type="text "
+                placeholder="Mical"
+                name="ln"
+                value={formData.ln}
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label>Email Address</Label>
-              <Input type="email" placeholder="JhonMical@example.com" />
+              <Input type="email"
+                placeholder="JhonMical@example.com"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label>Password</Label>
-              <Input type="password" placeholder="••••••••••••••••••" />
+              <Input type="password"
+                placeholder="••••••••••••••••••"
+                value={formData.password}
+                name="password"
+                onChange={handleChange}
+              />
+
             </div>
             <div className="flex justify-start space-x-3 items-center">
               <Checkbox className="" />
               <Label>Remember me</Label>
             </div>
-            <Button>Sign up</Button>
+            <Button
+              variant={`${loginButton ? "" : "disabled"}`}
+              disabled={!loginButton}
+              type="submit"
+            >Sign up</Button>
           </form>
         </div>
         <p>
